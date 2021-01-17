ea6158a445b63d4083ace9b347980bd4a4d4fd11
lvlv108 <vvlvennian@163.com>
2021-01-17 14:50:56 +0800

    调整目录

--- test_git/src/git_demo.py
+++ test_git/src/git_demo.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # @Time:2021/1/9 12:25 下午
 # @Author:lvlv
-# @File:demo.py.py
+# @File:git_demo.py.py
 
 
 print("hello from hogwarts@ceshiren.com!")
---
+++ test_python/hero.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time:2021/1/17 2:24 下午
+# @Author:lvlv TYC0811
+# @File:hero.py
+
+class hero:
+    pass
